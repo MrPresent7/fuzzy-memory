1a80732e69b0c0ae3b0019cce2b7a1e0f665cf4c
zolookor <119105006+zolookor@users.noreply.github.com>
2023-05-30 01:54:17 +0800

    -NoIdeawhatItis

--- My project (1)/Assets/Obstacle_spawn.cs
+++ My project (1)/Assets/Obstacle_spawn.cs
@@ -7,21 +7,20 @@ public class Obstacle_spawn : MonoBehaviour
     // Start is called before the first frame update
     public Rigidbody2D rb;
     public GameObject obstacle;
-    public Vector2 spawnPosition = new Vector2(0,0);
     public GameObject Mama;
     void Start()
     {
-        
+        Invoke("spawn", 3);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        Invoke("spawn", 3);
+        
     }
     void spawn()
     {
-        GameObject clone = Instantiate(obstacle, Mama.transform);
+        Instantiate(obstacle, Mama.transform.position, Quaternion.identity, Mama.transform);
     }
 }
