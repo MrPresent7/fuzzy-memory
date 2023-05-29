2fceab8ea003508797b2f5bd0227779a459ca477
zolookor <119105006+zolookor@users.noreply.github.com>
2023-05-30 00:45:36 +0800

    +obstacleMovemnt

--- My project (1)/Assets/Obstacle.cs
+++ My project (1)/Assets/Obstacle.cs
@@ -1,19 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public float Speed = -1f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.velocity = Vector2.left * Speed;
     }
 }
